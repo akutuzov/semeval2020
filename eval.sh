47664dc62ad60d267beeee8af52173149a3a57f5
mario <m.giulianelli.m@gmail.com>
2020-01-15 15:24:42 +0100

    change log format

--- eval.sh
+++ eval.sh
@@ -10,24 +10,26 @@ do
     true_answers1_path="trial_data_public/answer/task1/$language.txt"
     true_answers2_path="trial_data_public/answer/task2/$language.txt"
 
-    echo "Frequency Difference"
+    echo -e "\nFrequency Difference"
     unzip -oq results/answer_fd.zip
     python3 code/eval.py answer/task1/$language.txt answer/task2/$language.txt $true_answers1_path $true_answers2_path
     rm -r answer/
 
-    echo "Column Intersection + Cosine Distance"
+    echo -e "\nColumn Intersection + Cosine Distance"
     unzip -oq results/answer_cnt_ci_cd.zip
     python3 code/eval.py answer/task1/$language.txt answer/task2/$language.txt $true_answers1_path $true_answers2_path
     rm -r answer/
 
-    echo "Average Pairwise Distance"
+    echo -e "\nAverage Pairwise Distance"
     unzip -oq results/answer_bert_avg_pw_dist.zip
     python3 code/eval.py answer/task1/$language.txt answer/task2/$language.txt $true_answers1_path $true_answers2_path
     rm -r answer/
 
-    echo "Mean Relatedness Difference"
+    echo -e "\nMean Relatedness Difference"
     unzip -oq results/answer_bert_mean_rel_diff.zip
     python3 code/eval.py answer/task1/$language.txt answer/task2/$language.txt $true_answers1_path $true_answers2_path
     rm -r answer/
+
+    echo -e "\n"
 
 done
