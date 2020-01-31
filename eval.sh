4b23a0292e10001237598625083f4abdbe6ab6f6
mario <m.giulianelli.m@gmail.com>
2020-01-31 15:42:18 +0100

    Make project path variable for compatibility with server

--- eval.sh
+++ eval.sh
@@ -1,5 +1,8 @@
+# For compatibility with server
+project_path=$1
+
 # Unzip trial data
-unzip -oq trial_data_public.zip
+unzip -oq $project_path/trial_data_public.zip
 
 # Iterate over languages
 declare -a languages=(english german latin swedish)
@@ -7,28 +10,28 @@ for language in "${languages[@]}"
 do
     echo $language
 
-    true_answers1_path="trial_data_public/answer/task1/$language.txt"
-    true_answers2_path="trial_data_public/answer/task2/$language.txt"
+    true_answers1_path="$project_path/trial_data_public/answer/task1/$language.txt"
+    true_answers2_path="$project_path/trial_data_public/answer/task2/$language.txt"
 
     echo -e "\nFrequency Difference"
-    unzip -oq results/answer_fd.zip
-    python3 code/eval.py answer/task1/$language.txt answer/task2/$language.txt $true_answers1_path $true_answers2_path
-    rm -r answer/
+    unzip -oq $project_path/results/answer_fd.zip
+    python3 $project_path/code/eval.py $project_path/answer/task1/$language.txt $project_path/answer/task2/$language.txt $true_answers1_path $true_answers2_path
+    rm -r $project_path/answer/
 
     echo -e "\nColumn Intersection + Cosine Distance"
-    unzip -oq results/answer_cnt_ci_cd.zip
-    python3 code/eval.py answer/task1/$language.txt answer/task2/$language.txt $true_answers1_path $true_answers2_path
-    rm -r answer/
+    unzip -oq $project_path/results/answer_cnt_ci_cd.zip
+    python3 $project_path/code/eval.py $project_path/answer/task1/$language.txt $project_path/answer/task2/$language.txt $true_answers1_path $true_answers2_path
+    rm -r $project_path/answer/
 
     echo -e "\nAverage Pairwise Distance"
-    unzip -oq results/answer_bert_avg_pw_dist.zip
-    python3 code/eval.py answer/task1/$language.txt answer/task2/$language.txt $true_answers1_path $true_answers2_path
-    rm -r answer/
+    unzip -oq $project_path/results/answer_bert_avg_pw_dist.zip
+    python3 $project_path/code/eval.py $project_path/answer/task1/$language.txt $project_path/answer/task2/$language.txt $true_answers1_path $true_answers2_path
+    rm -r $project_path/answer/
 
     echo -e "\nMean Relatedness Difference"
-    unzip -oq results/answer_bert_mean_rel_diff.zip
-    python3 code/eval.py answer/task1/$language.txt answer/task2/$language.txt $true_answers1_path $true_answers2_path
-    rm -r answer/
+    unzip -oq $project_path/results/answer_bert_mean_rel_diff.zip
+    python3 $project_path/code/eval.py $project_path/answer/task1/$language.txt $project_path/answer/task2/$language.txt $true_answers1_path $true_answers2_path
+    rm -r $project_path/answer/
 
     echo -e "\n"
 
--- run-bert.sh
+++ run-bert.sh
@@ -1,9 +1,12 @@
+# For compatibility with server
+project_path=$1
+
 # Unzip trial data
-unzip -o trial_data_public.zip
+unzip -o $project_path/trial_data_public.zip
 
 # Make folders
-mkdir -p matrices/
-mkdir -p results/
+mkdir -p $project_path/matrices/
+mkdir -p $project_path/results/
 
 # Iterate over languages
 declare -a languages=(english german latin swedish)
@@ -11,34 +14,34 @@ for language in "${languages[@]}"
 do
 
     # Make folders
-    mkdir -p matrices/$language/
-    mkdir -p results/$language/
+    mkdir -p $project_path/matrices/$language/
+    mkdir -p $project_path/results/$language/
 
     # Collect contextualised representations
-    python3 code/bert/collect.py models/bert/$language/config.txt trial_data_public/corpora/$language/corpus1 trial_data_public/targets/$language.txt matrices/$language/bert_corpus1.dict
-    python3 code/bert/collect.py models/bert/$language/config.txt trial_data_public/corpora/$language/corpus2 trial_data_public/targets/$language.txt matrices/$language/bert_corpus2.dict
+    python3 $project_path/code/bert/collect.py $project_path/models/bert/$language/config.txt $project_path/trial_data_public/corpora/$language/corpus1 $project_path/trial_data_public/targets/$language.txt $project_path/matrices/$language/bert_corpus1.dict
+    python3 $project_path/code/bert/collect.py $project_path/models/bert/$language/config.txt $project_path/trial_data_public/corpora/$language/corpus2 $project_path/trial_data_public/targets/$language.txt $project_path/matrices/$language/bert_corpus2.dict
 
     # Compute diachronic average pairwise distance
-    python3 code/bert/distance.py trial_data_public/targets/$language.txt matrices/$language/bert_corpus1.dict matrices/$language/bert_corpus2.dict results/$language/bert_avg_pw_dist.csv
+    python3 $project_path/code/bert/distance.py $project_path/trial_data_public/targets/$language.txt $project_path/matrices/$language/bert_corpus1.dict $project_path/matrices/$language/bert_corpus2.dict $project_path/results/$language/bert_avg_pw_dist.csv
 
-    # Compute diachronic difference in mean relatedness
-    python3 code/bert/relatedness.py trial_data_public/targets/$language.txt matrices/$language/bert_corpus1.dict matrices/$language/bert_corpus2.dict results/$language/bert_mean_rel_diff.csv
+    # Compute diachronic (absolute) difference in mean relatedness
+    python3 $project_path/code/bert/relatedness.py -a $project_path/trial_data_public/targets/$language.txt $project_path/matrices/$language/bert_corpus1.dict $project_path/matrices/$language/bert_corpus2.dict $project_path/results/$language/bert_mean_rel_diff.csv
 
     # Classify results into two classes according to threshold
-    python3 code/class.py results/$language/bert_avg_pw_dist.csv results/$language/bert_avg_pw_dist_classes.csv 0.1
-    python3 code/class.py results/$language/bert_mean_rel_diff.csv results/$language/bert_mean_rel_diff_classes.csv 0.0
+    python3 $project_path/code/class.py $project_path/results/$language/bert_avg_pw_dist.csv $project_path/results/$language/bert_avg_pw_dist_classes.csv 0.5
+    python3 $project_path/code/class.py $project_path/results/$language/bert_mean_rel_diff.csv $project_path/results/$language/bert_mean_rel_diff_classes.csv 0.04
 
 
     ### Make answer files for submission ###
 
     # average pairwise distance
-    mkdir -p results/answer/task2/ && cp results/$language/bert_avg_pw_dist.csv results/answer/task2/$language.txt
-    mkdir -p results/answer/task1/ && cp results/$language/bert_avg_pw_dist_classes.csv results/answer/task1/$language.txt
-    cd results/ && zip -r answer_bert_avg_pw_dist.zip answer/ && rm -r answer/ && cd ..
+    mkdir -p $project_path/results/answer/task2/ && cp $project_path/results/$language/bert_avg_pw_dist.csv $project_path/results/answer/task2/$language.txt
+    mkdir -p $project_path/results/answer/task1/ && cp $project_path/results/$language/bert_avg_pw_dist_classes.csv $project_path/results/answer/task1/$language.txt
+    cd $project_path/results/ && zip -r $project_path/answer_bert_avg_pw_dist.zip $project_path/answer/ && rm -r $project_path/answer/ && cd $project_path/
 
     # difference in mean relatedness
-    mkdir -p results/answer/task2/ && cp results/$language/bert_mean_rel_diff.csv results/answer/task2/$language.txt
-    mkdir -p results/answer/task1/ && cp results/$language/bert_mean_rel_diff_classes.csv results/answer/task1/$language.txt
-    cd results/ && zip -r answer_bert_mean_rel_diff.zip answer/ && rm -r answer/ && cd ..
+    mkdir -p $project_path/results/answer/task2/ && cp $project_path/results/$language/bert_mean_rel_diff.csv $project_path/results/answer/task2/$language.txt
+    mkdir -p $project_path/results/answer/task1/ && cp $project_path/results/$language/bert_mean_rel_diff_classes.csv $project_path/results/answer/task1/$language.txt
+    cd $project_path/results/ && zip -r $project_path/answer_bert_mean_rel_diff.zip $project_path/answer/ && rm -r $project_path/answer/ && cd $project_path/
 
 done
