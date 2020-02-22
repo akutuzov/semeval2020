88d754532008433d6b4635adfe4ab46e3cbb537f
mario <m.giulianelli.m@gmail.com>
2020-02-22 15:48:05 +0100

    Fix paths

--- run-bert-evaluation.sh
+++ run-bert-evaluation.sh
@@ -13,29 +13,32 @@ cd $project_path || exit
 wget -q https://www2.ims.uni-stuttgart.de/data/sem-eval-ulscd/semeval2020_ulscd_eng.zip
 unzip -n semeval2020_ulscd_eng.zip
 rm -rf $data/english
-mv semeval2020_ulscd_eng  $data/english
+mv semeval2020_ulscd_eng  ${data:1}/english
 rm semeval2020_ulscd_eng.zip
 
 # German
 wget -q https://www2.ims.uni-stuttgart.de/data/sem-eval-ulscd/semeval2020_ulscd_ger.zip
 unzip -n semeval2020_ulscd_ger.zip
 rm -rf $data/german
-mv semeval2020_ulscd_ger  $data/german
+mv semeval2020_ulscd_ger  ${data:1}/german
 rm semeval2020_ulscd_ger.zip
 
 # Latin
 wget -q https://zenodo.org/record/3674099/files/semeval2020_ulscd_lat.zip
 unzip -n semeval2020_ulscd_lat.zip
 rm -rf $data/latin
-mv semeval2020_ulscd_lat  $data/latin
+mv semeval2020_ulscd_lat  ${data:1}/latin
 rm semeval2020_ulscd_lat.zip
 
 # Swedish
 wget -q https://zenodo.org/record/3672950/files/semeval2020_ulscd_swe.zip
 unzip -n semeval2020_ulscd_swe.zip
 rm -rf $data/swedish
-mv semeval2020_ulscd_swe  $data/swedish
+mv semeval2020_ulscd_swe  ${data:1}/swedish
 rm semeval2020_ulscd_swe.zip
+
+
+cd || exit
 
 
 # Make folders
