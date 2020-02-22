fc8acb90ee6441a7e34171ccaf5c417b6a300ecf
mario <m.giulianelli.m@gmail.com>
2020-02-22 15:28:49 +0100

    Add script for evaluation phase

---
+++ run-bert-evaluation.sh
@@ -0,0 +1,81 @@
+# For compatibility with server
+project_path=$1
+
+
+# Get data
+
+unzip -n $project_path/test_data_public.zip
+data=$project_path/test_data_public
+
+cd $project_path || exit
+
+# English
+wget https://www2.ims.uni-stuttgart.de/data/sem-eval-ulscd/semeval2020_ulscd_eng.zip
+unzip -n semeval2020_ulscd_eng.zip
+rm -rf $data/english
+mv semeval2020_ulscd_eng  $data/english
+rm semeval2020_ulscd_eng.zip
+
+# German
+wget https://www2.ims.uni-stuttgart.de/data/sem-eval-ulscd/semeval2020_ulscd_ger.zip
+unzip -n semeval2020_ulscd_ger.zip
+rm -rf $data/german
+mv semeval2020_ulscd_ger  $data/german
+rm semeval2020_ulscd_ger.zip
+
+# Latin
+wget https://zenodo.org/record/3674099/files/semeval2020_ulscd_lat.zip
+unzip -n semeval2020_ulscd_lat.zip
+rm -rf $data/latin
+mv semeval2020_ulscd_lat  $data/latin
+rm semeval2020_ulscd_lat.zip
+
+# Swedish
+wget https://zenodo.org/record/3672950/files/semeval2020_ulscd_swe.zip
+unzip -n semeval2020_ulscd_swe.zip
+rm -rf $data/swedish
+mv semeval2020_ulscd_swe  $data/swedish
+rm semeval2020_ulscd_swe.zip
+
+
+# Make folders
+mkdir -p $project_path/matrices_eval/
+mkdir -p $project_path/results_eval/
+
+# Iterate over languages
+declare -a languages=(english german latin swedish)
+for language in "${languages[@]}"
+do
+
+    # Make folders
+    mkdir -p $project_path/matrices_eval/$language/
+    mkdir -p $project_path/results_eval/$language/
+
+    # Collect contextualised representations
+    python3 $project_path/code/bert/collect.py $project_path/models/bert/$language/config.txt $project_path/trial_data_public/corpora/$language/corpus1 $project_path/trial_data_public/targets/$language.txt $project_path/matrices_eval/$language/bert_corpus1.dict
+    python3 $project_path/code/bert/collect.py $project_path/models/bert/$language/config.txt $project_path/trial_data_public/corpora/$language/corpus2 $project_path/trial_data_public/targets/$language.txt $project_path/matrices_eval/$language/bert_corpus2.dict
+
+    # Compute diachronic average pairwise distance
+    python3 $project_path/code/bert/distance.py $project_path/trial_data_public/targets/$language.txt $project_path/matrices_eval/$language/bert_corpus1.dict $project_path/matrices_eval/$language/bert_corpus2.dict $project_path/results_eval/$language/bert_avg_pw_dist.csv
+
+    # Compute diachronic (absolute) difference in mean relatedness
+    python3 $project_path/code/bert/relatedness.py -a $project_path/trial_data_public/targets/$language.txt $project_path/matrices_eval/$language/bert_corpus1.dict $project_path/matrices_eval/$language/bert_corpus2.dict $project_path/results_eval/$language/bert_mean_rel_diff.csv
+
+    # Classify results into two classes according to threshold
+    python3 $project_path/code/class.py $project_path/results_eval/$language/bert_avg_pw_dist.csv $project_path/results_eval/$language/bert_avg_pw_dist_classes.csv 0.5
+    python3 $project_path/code/class.py $project_path/results_eval/$language/bert_mean_rel_diff.csv $project_path/results_eval/$language/bert_mean_rel_diff_classes.csv 0.04
+
+
+    ### Make answer files for submission ###
+
+    # average pairwise distance
+    mkdir -p $project_path/results_eval/answer/task2/ && cp $project_path/results_eval/$language/bert_avg_pw_dist.csv $project_path/results_eval/answer/task2/$language.txt
+    mkdir -p $project_path/results_eval/answer/task1/ && cp $project_path/results_eval/$language/bert_avg_pw_dist_classes.csv $project_path/results_eval/answer/task1/$language.txt
+    zip -r $project_path/results_eval/answer_bert_avg_pw_dist.zip $project_path/results_eval/answer/ && rm -r $project_path/results_eval/answer/
+
+    # difference in mean relatedness
+    mkdir -p $project_path/results_eval/answer/task2/ && cp $project_path/results_eval/$language/bert_mean_rel_diff.csv $project_path/results_eval/answer/task2/$language.txt
+    mkdir -p $project_path/results_eval/answer/task1/ && cp $project_path/results_eval/$language/bert_mean_rel_diff_classes.csv $project_path/results_eval/answer/task1/$language.txt
+    zip -r $project_path/results_eval/answer_bert_mean_rel_diff.zip $project_path/results_eval/answer/ && rm -r $project_path/results_eval/answer/
+
+done
