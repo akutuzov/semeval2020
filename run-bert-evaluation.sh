d9be0dae645f9fabcf5186db3d7503c4e10f30d4
mario <m.giulianelli.m@gmail.com>
2020-02-22 15:33:56 +0100

    Make wget quiet

--- run-bert-evaluation.sh
+++ run-bert-evaluation.sh
@@ -10,28 +10,28 @@ data=$project_path/test_data_public
 cd $project_path || exit
 
 # English
-wget https://www2.ims.uni-stuttgart.de/data/sem-eval-ulscd/semeval2020_ulscd_eng.zip
+wget -q https://www2.ims.uni-stuttgart.de/data/sem-eval-ulscd/semeval2020_ulscd_eng.zip
 unzip -n semeval2020_ulscd_eng.zip
 rm -rf $data/english
 mv semeval2020_ulscd_eng  $data/english
 rm semeval2020_ulscd_eng.zip
 
 # German
-wget https://www2.ims.uni-stuttgart.de/data/sem-eval-ulscd/semeval2020_ulscd_ger.zip
+wget -q https://www2.ims.uni-stuttgart.de/data/sem-eval-ulscd/semeval2020_ulscd_ger.zip
 unzip -n semeval2020_ulscd_ger.zip
 rm -rf $data/german
 mv semeval2020_ulscd_ger  $data/german
 rm semeval2020_ulscd_ger.zip
 
 # Latin
-wget https://zenodo.org/record/3674099/files/semeval2020_ulscd_lat.zip
+wget -q https://zenodo.org/record/3674099/files/semeval2020_ulscd_lat.zip
 unzip -n semeval2020_ulscd_lat.zip
 rm -rf $data/latin
 mv semeval2020_ulscd_lat  $data/latin
 rm semeval2020_ulscd_lat.zip
 
 # Swedish
-wget https://zenodo.org/record/3672950/files/semeval2020_ulscd_swe.zip
+wget -q https://zenodo.org/record/3672950/files/semeval2020_ulscd_swe.zip
 unzip -n semeval2020_ulscd_swe.zip
 rm -rf $data/swedish
 mv semeval2020_ulscd_swe  $data/swedish
