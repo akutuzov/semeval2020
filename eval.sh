6d3c52924fd283be0cacb892e0b7ab4279f63a2c
mario <m.giulianelli.m@gmail.com>
2020-02-22 14:53:14 +0100

    Make zipping not quiet

--- eval.sh
+++ eval.sh
@@ -14,22 +14,22 @@ do
     true_answers2_path="$project_path/trial_data_public/answer/task2/$language.txt"
 
 #    echo -e "\nFrequency Difference"
-#    unzip -oq $project_path/results/answer_fd.zip
+#    unzip -o $project_path/results/answer_fd.zip
 #    python3 $project_path/code/eval.py $project_path/results/answer/task1/$language.txt $project_path/results/answer/task2/$language.txt $true_answers1_path $true_answers2_path
 #    rm -r $project_path/answer/
 #
 #    echo -e "\nColumn Intersection + Cosine Distance"
-#    unzip -oq $project_path/results/answer_cnt_ci_cd.zip
+#    unzip -o $project_path/results/answer_cnt_ci_cd.zip
 #    python3 $project_path/code/eval.py $project_path/results/answer/task1/$language.txt $project_path/results/answer/task2/$language.txt $true_answers1_path $true_answers2_path
 #    rm -r $project_path/answer/
 
     echo -e "\nAverage Pairwise Distance"
-    unzip -oq $project_path/results/answer_bert_avg_pw_dist.zip
+    unzip -o $project_path/results/answer_bert_avg_pw_dist.zip
     python3 $project_path/code/eval.py $project_path/results/answer/task1/$language.txt $project_path/results/answer/task2/$language.txt $true_answers1_path $true_answers2_path
     rm -r $project_path/answer/
 
     echo -e "\nMean Relatedness Difference"
-    unzip -oq $project_path/results/answer_bert_mean_rel_diff.zip
+    unzip -o $project_path/results/answer_bert_mean_rel_diff.zip
     python3 $project_path/code/eval.py $project_path/results/answer/task1/$language.txt $project_path/results/answer/task2/$language.txt $true_answers1_path $true_answers2_path
     rm -r $project_path/answer/
 
