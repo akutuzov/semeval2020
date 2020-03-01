7a19e1c6233770be82e0d555dc58f06f05188616
Andrey Kutuzov <akutuzov72@gmail.com>
2020-03-02 00:21:48 +0100

    Better script to run ELMo

--- run_elmo.sh
+++ run_elmo.sh
@@ -10,6 +10,7 @@ FILENAME2=$(basename -- "$CORPUS2" .txt.gz)
 ELMO1=${3}
 ELMO2=${4}
 TARGET=${5}
+LANG=${6}
 
 echo 'Inferring embeddings for the corpus 1...'
 python3 code/elmo/extract_elmo.py --input ${CORPUS1} --elmo ${ELMO1} --outfile ${FILENAME1}.npz  --vocab ${TARGET}
@@ -19,3 +20,7 @@ python3 code/elmo/extract_elmo.py --input ${CORPUS2} --elmo ${ELMO2} --outfile $
 
 echo 'Calculating diversity coefficients...'
 python3 code/elmo/get_coeffs.py -i0 ${FILENAME1}.npz -i1 ${FILENAME2}.npz > ${FILENAME1}_${FILENAME2}.tsv
+
+echo 'Preparing a submission...'
+mkdir -p results_elmo/answer/task2/ && mv ${FILENAME1}_${FILENAME2}.tsv results_elmo/answer/task2/$LANG.txt
+zip -r results_elmo/answer.zip results_elmo/answer/ && rm -r results_elmo/answer/
