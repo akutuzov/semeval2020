0ae61feb37fdc6520946a35f3c09e7baaf901335
mario <m.giulianelli.m@gmail.com>
2020-03-23 15:26:23 +0100

    Minor

--- eval_bert_task2.sh
+++ eval_bert_task2.sh
@@ -2,24 +2,24 @@
 project_path=$1
 cd $project_path || exit
 
-declare -a languages=(english latin german swedish)
+declare -a languages=(english latin german) # swedish)
 declare -a methods=(avg last last4 mid4)
 declare -a metrics=(_apd _amrd _jsd _pdiv _cdiv -mean_cosine)
 
 for language in "${languages[@]}"
 do
-  echo $language
+  echo ">>>" $language "<<<"
   gold1=test_data_truth/task1/${language}.txt
   gold2=test_data_truth/task2/${language}.txt
   out=results_eval/$language
 
   for method in "${methods[@]}"
   do
-    echo $method
+    echo ">>" $method "<<"
 
     for metric in "${metrics[@]}"
     do
-	    echo $metric
+	    echo '>' $metric '<'
       python3 code/eval.py ${out}/bert${metric}_${method}.csv ${out}/bert${metric}_${method}.csv $gold1 $gold2
     done
 
