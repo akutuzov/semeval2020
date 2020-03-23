eae5ad92273c871e989949d548e4059780737d3f
mario <m.giulianelli.m@gmail.com>
2020-03-23 15:12:44 +0100

    Script to evaluate BERT predictions for Task 2

---
+++ eval_bert_task2.sh
@@ -0,0 +1,25 @@
+# Provide project path as argument to this script!
+project_path=$1
+cd $project_path || exit
+
+declare -a languages=(english latin german swedish)
+declare -a methods=(avg last last4 mid4)
+declare -a metrics=(_apd _amrd _jsd _pdiv _cdiv -mean_cosine)
+
+for language in "${languages[@]}"
+do
+  for method in "${methods[@]}"
+  do
+
+    gold1=test_data_truth/task1/${language}.txt
+    gold2=test_data_truth/task2/${language}.txt
+    out=results_eval/$language
+
+    for metric in "${metrics[@]}"
+    do
+	    echo $metric
+      python3 code/eval.py ${out}/bert${metric}_${method}.csv $gold1 $gold2
+    done
+
+	done
+done
