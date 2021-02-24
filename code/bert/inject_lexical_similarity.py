Fix embedding normalisation

code/bert/inject_lexical_similarity.py
@@ -244,9 +244,12 @@ def main():
             hidden_states = outputs[1]
             last_layer = hidden_states[-1][np.arange(bsz), positions, :]  # (bsz, hdims)
             if args.normalise_embeddings:
-                last_layer /= last_layer.sum()
+                last_layer /= last_layer.sum(1, keepdim=True)
 
             dot_products = torch.sum(tgt_embedding * last_layer, dim=1)  # (bsz)
+
+            if args.normalise_embeddings:
+                assert all([d <= 1 for d in dot_products]), 'Dot product should not exceed 1 if vectors are normalised.'
 
             for b_id in np.arange(bsz):
                 tgt_lemma = tgt[b_id]