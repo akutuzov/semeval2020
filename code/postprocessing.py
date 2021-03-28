d0fd6441d816bd66217fb333b0bfe6f9e6bc65bf
Andrey Kutuzov <akutuzov72@gmail.com>
2021-03-28 23:58:03 +0200

    Added UDPipe to the lemmatizer Fixes #2

--- code/postprocessing.py
+++ code/postprocessing.py
@@ -7,22 +7,33 @@ from smart_open import open
 import numpy as np
 from collections import defaultdict
 from wordfreq import word_frequency
-
+from ufal.udpipe import Model, Pipeline
 
 logger = logging.getLogger(__name__)
 
 
+def lemm_udpipe(pipeline, word):
+    # lemmatizing and processing the resulting CONLLU
+    processed = pipeline.process(word)
+    content = [line for line in processed.split("\n") if not line.startswith("#")]
+    tagged = [w.split("\t") for w in content if w]
+    (word_id, token, lemma, pos, xpos, feats, head, deprel, deps, misc) = tagged[0]
+    return lemma
+
+
 def main():
     """
-    Correct probabilities with lexical similarity scores, correct for substitute word frequency, lemmatise,
-    and filter out redundant candidates.
+    Correct probabilities with lexical similarity scores, correct for substitute word frequency,
+    lemmatise, and filter out redundant candidates.
     """
     parser = argparse.ArgumentParser(
-        description='Correct probabilities with lexical similarity scores, correct for substitute word frequency, '
+        description='Correct probabilities with lexical similarity scores, '
+                    'correct for substitute word frequency, '
                     'lemmatise, and filter out redundant candidates.')
     parser.add_argument(
         '--subs_path', type=str, required=True,
-        help='Path to the pickle file containing substitute lists (output by inject_word_similarity.py).')
+        help='Path to the pickle file containing substitute lists '
+             '(output by inject_word_similarity.py).')
     parser.add_argument(
         '--output_path', type=str, required=True,
         help='Output path for pickle containing substitutes with updated log probabilities.')
@@ -45,14 +56,20 @@ def main():
         '--k', type=float, default=4,
         help='The value of parameter k in the prior word probability distribution.')
     parser.add_argument(
-        '--s', type = float, default = 1.05,
-        help = 'The value of parameter s in the prior word probability distribution.')
+        '--s', type=float, default=1.05,
+        help='The value of parameter s in the prior word probability distribution.')
     parser.add_argument(
         '--beta', type=float, default=2,
         help='The value of parameter beta.')
     parser.add_argument(
         '--frequency_list', type=str,
-        help='Path to a frequency list tsv file (word\tfreq[\trank]\n) to use instead of the wordfreq library.')
+        help='Path to a frequency list tsv file (word\tfreq[\trank]\n) '
+             'to use instead of the wordfreq library.')
+    parser.add_argument('--lemmatizer', choices=["stanza", "udpipe"], default="stanza",
+                        help='The lemmatizer to use.')
+    parser.add_argument('--udfile', default="english-lines-ud-2.5-191206.udpipe",
+                        help='UDPipe model to use. '
+                             'https://lindat.mff.cuni.cz/repository/xmlui/handle/11234/1-3131')
     args = parser.parse_args()
 
     lang = args.lang.lower()
@@ -66,7 +83,8 @@ def main():
     for lemma in substitutes_pre:
         for occurrence in substitutes_pre[lemma]:
 
-            # log p(c_j|w,s_i) = log p(c_j|s_i) + log p(c_j|w), with p(c_j|w) = exp(dot(emb_c_j, embed_w))
+            # log p(c_j|w,s_i) = log p(c_j|s_i) + log p(c_j|w),
+            # with p(c_j|w) = exp(dot(emb_c_j, embed_w))
             for i, dotp in enumerate(occurrence['dot_products']):
                 occurrence['logp'][i] += dotp / args.temperature
                 # occurrence['logp'][i] = Decimal(occurrence['logp'][i])
@@ -76,9 +94,9 @@ def main():
             occurrence['logp'] = [occurrence['logp'][j] for j in indices]
             occurrence['candidate_words'] = [occurrence['candidate_words'][j] for j in indices]
 
-
     if args.frequency_correction:
         logger.warning('Correct for word frequency.')
+        log_prior = None
         if args.frequency_list:
             logger.warning('Loading frequency list.')
             freqs_tmp = dict()
@@ -103,22 +121,26 @@ def main():
 
         for target in substitutes_pre:
             for occurrence in substitutes_pre[target]:
-                prior_prob = []
+                prior_prob = []  # TODO: what is this for?
                 for w, logp in zip(occurrence['candidate_words'], occurrence['logp']):
 
                     if args.frequency_list:
                         logp -= args.beta * log_prior[w]
                     else:
-                        logp -= args.beta * np.log(word_frequency(w, lang, wordlist='best') ** args.s)
-
+                        logp -= args.beta * np.log(
+                            word_frequency(w, lang, wordlist='best') ** args.s)
 
     if args.lemmatise:
         logger.warning('Lemmatisation postprocessing.')
-        try:
-            nlp = stanza.Pipeline(lang=lang, processors='tokenize, lemma')
-        except Exception:
-            stanza.download(lang=lang, processors='tokenize, lemma')
-            nlp = stanza.Pipeline(lang=lang, processors='tokenize, lemma')
+        if args.lemmatizer == "udpipe":
+            lemm_model = Model.load(args.udfile)
+            nlp = Pipeline(lemm_model, "tokenize", Pipeline.DEFAULT, Pipeline.DEFAULT, "conllu")
+        else:
+            try:
+                nlp = stanza.Pipeline(lang=lang, processors='tokenize, lemma')
+            except Exception:
+                stanza.download(lang=lang, processors='tokenize, lemma')
+                nlp = stanza.Pipeline(lang=lang, processors='tokenize, lemma')
 
         substitutes_post = {
             w: [{'candidate_words': [], 'logp': []} for _ in substitutes_pre[w]]
@@ -126,13 +148,19 @@ def main():
         }
 
         for target in substitutes_pre:
-            tgt_lemma = nlp(target).sentences[0].words[0].lemma
+            if args.lemmatizer == "udpipe":
+                tgt_lemma = lemm_udpipe(nlp, target)
+            else:
+                tgt_lemma = nlp(target).sentences[0].words[0].lemma
             for i, occurrence in enumerate(substitutes_pre[target]):
                 subs_lemmas = {}
 
                 j = 0
                 for sub, sub_logp in zip(occurrence['candidate_words'], occurrence['logp']):
-                    sub_lemma = nlp(sub).sentences[0].words[0].lemma
+                    if args.lemmatizer == "udpipe":
+                        sub_lemma = lemm_udpipe(nlp, sub)
+                    else:
+                        sub_lemma = nlp(sub).sentences[0].words[0].lemma
 
                     if len(sub_lemma) <= 1:
                         continue
@@ -142,7 +170,8 @@ def main():
 
                     if sub_lemma in subs_lemmas:
                         p = np.exp(occurrence['logp'][subs_lemmas[sub_lemma]]) + np.exp(sub_logp)
-                        substitutes_post[target][i]['logp'][subs_lemmas[sub_lemma]] = np.log(p)  #.ln()
+                        substitutes_post[target][i]['logp'][subs_lemmas[sub_lemma]] = np.log(
+                            p)  # .ln()
                     else:
                         subs_lemmas[sub_lemma] = j
                         substitutes_post[target][i]['candidate_words'].append(sub_lemma)
@@ -161,7 +190,7 @@ def main():
                 occurrence['candidate_words'] = occurrence['candidate_words'][:args.n_subs]
 
             # re-normalise
-            log_denominator = np.log(np.sum(np.exp(occurrence['logp'])))  #.ln()
+            log_denominator = np.log(np.sum(np.exp(occurrence['logp'])))  # .ln()
             for logp in occurrence['logp']:
                 logp -= log_denominator
 
