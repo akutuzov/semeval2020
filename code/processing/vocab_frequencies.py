9b9079b3614656f702a941cb57fc08bb329dc626
mario <m.giulianelli.m@gmail.com>
2021-04-02 14:07:41 +0200

    Add lowercasing option and start ranks from 1

--- code/processing/vocab_frequencies.py
+++ code/processing/vocab_frequencies.py
@@ -1,3 +1,4 @@
+import argparse
 from collections import defaultdict
 from gensim.models.word2vec import PathLineSentences
 from docopt import docopt
@@ -7,35 +8,32 @@ from smart_open import open
 
 
 def main():
-    """
-    Get frequencies from corpus.
-    """
 
-    # Get the arguments
-    args = docopt("""Get frequencies from corpus.
-
-    Usage:
-        freq.py [-n] <corpDir> <outPath>
-
-    Arguments:
-        <corpDir> = path to corpus or corpus directory (iterates through files)
-        <outPath> = output path for result file
-
-    Options:
-        -n --norm  normalize frequency by total corpus frequency
-
-    """)
-
-    is_norm = args['--norm']
-    corpDir = args['<corpDir>']
-    outPath = args['<outPath>']
+    parser = argparse.ArgumentParser(description='Get frequencies from corpus.')
+    parser.add_argument(
+        '--data_path', type=str, required=True,
+        help='Path to corpus or corpus directory (iterates through files).'
+    )
+    parser.add_argument(
+        '--output_path', type=str, required=True,
+        help='Output path for result file.'
+    )
+    parser.add_argument(
+        '--norm', action='store_true',
+        help='Normalize frequency by total corpus frequency.'
+    )
+    parser.add_argument(
+        '--lower', action='store_true',
+        help='Apply lowercasing to the corpus.'
+    )
+    args = parser.parse_args()
 
     logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
     logging.info(__file__.upper())
     start_time = time.time()
 
     # Get sentence iterator
-    sentences = PathLineSentences(corpDir)
+    sentences = PathLineSentences(args.data_path)
 
     # Initialize frequency dictionary
     freqs = defaultdict(int)
@@ -45,20 +43,21 @@ def main():
     for sentence in sentences:
         for word in sentence:
             corpusSize += 1
-            freqs[word] = freqs[word] + 1
+            if args.lower:
+                freqs[word.lower()] = freqs[word.lower()] + 1
+            else:
+                freqs[word] = freqs[word] + 1
 
     freqs = dict(sorted(freqs.items(), key=lambda item: item[1], reverse=True))
 
     # Write frequency scores
-    with open(outPath, 'w', encoding='utf-8') as f_out:
-        for i, word in enumerate(freqs):
-            if is_norm:
+    with open(args.output_path, 'w', encoding='utf-8') as f_out:
+        for i, word in enumerate(freqs, start=1):
+            if args.norm:
                 freqs[word] = float(freqs[word]) / corpusSize  # Normalize by total corpus frequency
                 f_out.write('\t'.join((word, str(freqs[word]), str(i))) + '\n')
             else:
                 f_out.write('\t'.join((word, str(freqs[word]), str(i))) + '\n')
-
-
 
     logging.info('tokens: %d' % (corpusSize))
     logging.info('types: %d' % (len(freqs.keys())))
