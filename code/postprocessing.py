Fix frequency weighting

code/postprocessing.py
@@ -35,7 +35,8 @@ def main():
                     'lemmatise, and filter out redundant candidates.')
     parser.add_argument(
         '--subs_path', type=str, required=True,
-        help='Path to the pickle file containing substitute lists (output by inject_word_similarity.py).'
+        help='Path to the pickle file containing substitute lists '
+             '(output by inject_word_similarity.py).'
     )
     parser.add_argument(
         '--output_path', type=str, required=True,
@@ -111,7 +112,6 @@ def main():
     else:
         raise ValueError('Invalid path: {}'.format(args.subs_path))
 
-
     start_time = time.time()
 
     for lemma in substitutes_pre:
@@ -145,12 +145,12 @@ def main():
 
         for target in substitutes_pre:
             for occurrence in substitutes_pre[target]:
-                for w, logp in zip(occurrence['candidate_words'], occurrence['logp']):
-
+                for nr, (w, logp) in enumerate(zip(occurrence['candidate_words'],
+                                                   occurrence['logp'])):
                     if args.frequency_list:
-                        logp -= args.beta * log_prior[w]
+                        occurrence['logp'][nr] -= args.beta * log_prior[w]
                     else:
-                        logp -= args.beta * np.log(
+                        occurrence['logp'][nr] -= args.beta * np.log(
                             word_frequency(w, args.lang, wordlist='best') ** args.s)
 
     if args.lemmatise:
@@ -213,13 +213,13 @@ def main():
 
             # re-normalise
             log_denominator = np.log(np.sum(np.exp(occurrence['logp'])))  # .ln()
-            for logp in occurrence['logp']:
-                logp -= log_denominator
+            for nr, logp in enumerate(occurrence['logp']):
+                occurrence['logp'][nr] -= log_denominator
 
     for target in substitutes_post:
         for occurrence in substitutes_post[target]:
-            for logp in occurrence['logp']:
-                logp = float(logp)
+            for nr, logp in enumerate(occurrence['logp']):
+                occurrence['logp'][nr] = float(logp)
 
     for word in substitutes_post:
         if len(substitutes_post[word]) < 1:

code/wsi_jsd.py
@@ -10,7 +10,6 @@ import numpy as np
 from scipy.stats import entropy
 from smart_open import open
 
-
 logger = logging.getLogger(__name__)
 
 
@@ -29,10 +28,12 @@ def main():
         description='Word sense induction via agglomerative clustering of lexical substitutes.')
     parser.add_argument(
         '--subs_path_t1', type=str, required=True,
-        help='Path to the pickle file containing substitute lists (output by postprocessing.py) for period T1.')
+        help='Path to the pickle file containing substitute lists '
+             '(output by postprocessing.py) for period T1.')
     parser.add_argument(
         '--subs_path_t2', type=str, required=True,
-        help='Path to the pickle file containing substitute lists (output by postprocessing.py) for period T2.')
+        help='Path to the pickle file containing substitute lists '
+             '(output by postprocessing.py) for period T2.')
     parser.add_argument(
         '--targets_path', type=str, required=True,
         help='Path to the csv file containing target word forms.')
@@ -83,7 +84,6 @@ def main():
     else:
         raise ValueError('Invalid path: {}'.format(args.subs_path))
 
-
     # Load target forms
     targets = []
     with open(args.targets_path, 'r', encoding='utf-8') as f_in:
@@ -118,7 +118,8 @@ def main():
         except KeyError:
             logger.warning('No occurrences of {} in T2.'.format(target))
 
-    logger.warning('\nCollected vocabularies for {} targets.'.format(len([n for n in n_occurrences.values() if n > 0])))
+    logger.warning(f"Collected vocabularies for "
+                   f"{len([n for n in n_occurrences.values() if n > 0])} targets.")
     logger.warning('Total occurrences: {}'.format(sum(n_occurrences.values())))
     logger.warning('Minimum vocabulary size: {}'.format(min([len(v) for v in vocabs.values()])))
     logger.warning('Maximum vocabulary size: {}\n'.format(max([len(v) for v in vocabs.values()])))
@@ -129,13 +130,18 @@ def main():
 
         if len(vocabs[target]) == 0:
             jsd_scores[target] = 1.
-            logger.warning('Assigning JSD=1 to target word: {}. No substitutes available.'.format(target))
+            logger.warning(f"Assigning JSD=1 to target word: {target}. No substitutes available.")
+            continue
+        if target not in substitutes_t1 or target not in substitutes_t2:
+            jsd_scores[target] = 1.
+            logger.warning(f"Assigning JSD=1 to target word: {target}. "
+                           f"No occurrences in at least one corpus")
             continue
 
         # for each target, construct a one-hot matrix M where
         # cell M[i,j] encodes whether substitute j is in the
         # list of substitutes generated for sentence i
-        logger.warning('Construct matrix.')
+        logger.warning('Constructing the matrix...')
         w2i = {w: i for i, w in enumerate(vocabs[target])}
         m = np.zeros((n_occurrences[target], len(vocabs[target])))
 
@@ -154,11 +160,11 @@ def main():
         assert occ_idx == n_occurrences[target]
 
         if args.apply_tfidf:
-            logger.warning('Apply tf-idf.')
+            logger.warning('Applying tf-idf...')
             tfidf = TfidfTransformer()
             m = tfidf.fit_transform(m).toarray()
 
-        logger.warning('Cluster into {} cluster.'.format(args.n_clusters))
+        logger.warning('Clustering into {} clusters...'.format(args.n_clusters))
         clustering = AgglomerativeClustering(
             n_clusters=args.n_clusters,
             affinity=args.affinity,