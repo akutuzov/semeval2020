9cbd10cc16be851ec1457ba8a68c642e84e8c140
Pivovarova Lidia <lidia.pivovarova@helsinki.fi>
2021-05-23 20:33:16 +0300

    ENH: percentage threshold

--- code/ling_features/generate_preds.sh
+++ code/ling_features/generate_preds.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
 LANG=${1}
+THR=${2}
 
 for feat in morph synt
 do
-    python3 compare_ling.py --input1 ../../data/features/${LANG}/corpus1_${feat}.json --input2 ../../data/features/${LANG}/corpus2_${feat}.json --output results/${LANG}_${feat}
+    python3 compare_ling.py --input1 ../../data/features/${LANG}/corpus1_${feat}.json --input2 ../../data/features/${LANG}/corpus2_${feat}.json --output results/${LANG}_${feat}_${THR} --threshold ${THR}
 done
 
 for task in binary graded
 do
-    python3 merge.py -i1 results/${LANG}_morph_${task}.tsv -i2 results/${LANG}_synt_${task}.tsv > results/${LANG}_${task}.tsv
+    python3 merge.py -i1 results/${LANG}_morph_${THR}_${task}.tsv -i2 results/${LANG}_synt_${THR}_${task}.tsv > results/${LANG}_${task}_${THR}.tsv
 done
--- code/ling_features/mass_eval.sh
+++ code/ling_features/mass_eval.sh
@@ -1,12 +1,13 @@
 #! /bin/bash
 
 LANG=${1}
+THR=${2}
 
 for feat in morph synt
 do
     echo "Evaluating ${feat}"
-    python3 eval.py results/${LANG}_${feat}_binary.tsv results/${LANG}_${feat}_graded.tsv gold/task1/${LANG}.txt gold/task2/${LANG}.txt
+    python3 ../eval.py results/${LANG}_${feat}_${THR}_binary.tsv results/${LANG}_${feat}_${THR}_graded.tsv ../../test_data_truth/task1/${LANG}.txt ../../test_data_truth/task2/${LANG}.txt
 done
 
 echo "Evaluating averaged"
-python3 eval.py results/${LANG}_binary.tsv results/${LANG}_graded.tsv gold/task1/${LANG}.txt gold/task2/${LANG}.txt
+python3 ../eval.py results/${LANG}_binary_${THR}.tsv results/${LANG}_graded_${THR}.tsv ../../test_data_truth/task1/${LANG}.txt ../../test_data_truth/task2/${LANG}.txt
