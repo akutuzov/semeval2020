a43572f2411588886238b1de6abf8591bc81f131
Andrey Kutuzov <akutuzov72@gmail.com>
2021-05-27 14:21:29 +0200

    Code cleanup

--- code/ling_features/compare_ling.py
+++ code/ling_features/compare_ling.py
@@ -16,35 +16,32 @@ informative = ["nominal_function",
                "core_nominals",
                "nominal_dependents"]
 
-
 groups = json.load(open("../../data/features/synt_groups.json", "r"))
 feature_to_group = {}
-for k,v in groups.items():
+for k, v in groups.items():
     for f in v:
         feature_to_group[f] = k
 
 
-
 def update(properties, filtering):
     new_properties = defaultdict(int)
-    for feature,count in properties.items():
-        group = feature_to_group[feature.split(":")[0]]
+    for current_feature, count in properties.items():
+        group = feature_to_group[current_feature.split(":")[0]]
         if filtering == "group":
             new_properties[group] += count
         elif filtering == "partial":
-            group = feature_to_group[feature.split(":")[0]]
+            group = feature_to_group[current_feature.split(":")[0]]
             if group in informative:
-                new_properties[feature] = count
+                new_properties[current_feature] = count
             else:
                 new_properties[group] += count
         elif filtering == "delete":
             if group in informative:
-                new_properties[feature] = count
+                new_properties[current_feature] = count
         else:
             raise NotImplementedError
     return new_properties
-        
-    
+
 
 if __name__ == "__main__":
     logging.basicConfig(
@@ -57,25 +54,26 @@ if __name__ == "__main__":
     arg("--input1", "-i1", help="Path to a JSON file 1", required=True)
     arg("--input2", "-i2", help="Path to a JSON file 2", required=True)
     arg("--output", "-o", help="Output path (tsv)", required=False)
-    arg("--threshold", "-t", nargs='?', const=0, help="Minimal percentage to keep a feature", default=0, type=int, required=False)
-    arg("--filtering", "-f", help="Organizing syntactic features according to UD classification: 'group' - grouping all, 'delete' - deleting non-informative, 'partial' - grouping non-informative", choices=["group", "partial", "delete", "none"], default="none")
-    arg("--distance", "-d", help="Choose between cosine and jsd", choices=["cos", "jsd"], default="cos")
-    
+    arg("--threshold", "-t", nargs='?', const=0, help="Minimal percentage to keep a feature",
+        default=0, type=int, required=False)
+    arg("--filtering", "-f", help="Organizing syntactic features according to UD classification: "
+                                  "'group' - grouping all, 'delete' - deleting non-informative, "
+                                  "'partial' - grouping non-informative",
+        choices=["group", "partial", "delete", "none"], default="none")
+    arg("--distance", "-d", help="Choose between cosine and jsd", choices=["cos", "jsd"],
+        default="cos")
+
     args = parser.parse_args()
 
-    #print("ARGS: %s" %args)
-        
     with open(args.input1, "r") as f:
         properties_1 = json.loads(f.read())
-            
+
     with open(args.input2, "r") as f:
         properties_2 = json.loads(f.read())
 
     assert properties_1.keys() == properties_2.keys()
     words = {w: 0 for w in properties_1.keys()}
 
-
-    
     all_features = set()
 
     for word in words:
@@ -86,16 +84,15 @@ if __name__ == "__main__":
         if args.filtering != "none":
             p1 = update(p1, args.filtering)
             p2 = update(p2, args.filtering)
-                   
+
         features = list(p1.keys() | p2.keys())
-        
-        prop_count = {k:p1.get(k,0)+p2.get(k,0) for k in features}
+
+        prop_count = {k: p1.get(k, 0) + p2.get(k, 0) for k in features}
         total = sum(prop_count.values())
-        features = [f for f in features if prop_count[f]/total*100 > args.threshold]
-            
-        all_features.update(features)            
-        
-                
+        features = [f for f in features if prop_count[f] / total * 100 > args.threshold]
+
+        all_features.update(features)
+
         vector_1 = np.zeros(len(features))
         vector_2 = np.zeros(len(features))
 
@@ -114,15 +111,11 @@ if __name__ == "__main__":
         elif args.distance == "jsd":
             distance = jensenshannon(vector_1, vector_2)
         else:
-            raise NotImplementedError("Unknown distance: %s" %args.distance)
-
+            raise NotImplementedError("Unknown distance: %s" % args.distance)
 
         if np.isnan(distance):
             distance = 0.0  # A word was not present in one of the time periods
         words[word] = distance
-
-        
-    #print(all_features)
 
     if args.output:
         with open(f"{args.output}_graded.tsv", "w") as f:
