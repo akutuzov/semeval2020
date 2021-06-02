be5a61079337ba0dd4ed2888dc84fa823e02d2c7
Pivovarova Lidia <lidia.pivovarova@helsinki.fi>
2021-06-02 13:24:32 +0300

    ENH: morphological feature separation with separate distances

---
+++ code/ling_features/evaluate_morph_separation.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+LANG=${1}
+
+for distance in cos jsd
+do
+    echo
+    echo ${distance}
+    for agregate in max avg
+    do
+	echo ${agregate}
+	python3 -W ignore compare_morph_separately.py --input1 ../../data/features/${LANG}/corpus1_morph.json --input2 ../../data/features/${LANG}/corpus2_morph.json --output results/${LANG}_separate_${distance}_${agregate} --distance ${distance} --agregate ${agregate}
+
+	python3 ../eval.py results/${LANG}_separate_${distance}_${agregate}_binary.tsv results/${LANG}_separate_${distance}_${agregate}_graded.tsv ../../test_data_truth/task1/${LANG}.txt ../../test_data_truth/task2/${LANG}.txt
+	
+	echo thr 5
+    
+	python3 -W ignore compare_morph_separately.py --input1 ../../data/features/${LANG}/corpus1_morph.json --input2 ../../data/features/${LANG}/corpus2_morph.json --output results/${LANG}_separate_${distance}_${agregate}_5 --threshold 5 --distance ${distance} --agregate ${agregate}
+
+	python3 ../eval.py results/${LANG}_separate_${distance}_${agregate}_5_binary.tsv results/${LANG}_separate_${distance}_${agregate}_5_graded.tsv ../../test_data_truth/task1/${LANG}.txt ../../test_data_truth/task2/${LANG}.txt
+
+    echo
+    done
+done
+
+       
+    
+	
+     
