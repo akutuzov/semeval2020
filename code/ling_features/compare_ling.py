8e54203274b71f01b01321c8cd80a6f214ea6896
Pivovarova Lidia <lidia.pivovarova@helsinki.fi>
2021-06-04 15:44:01 +0300

    REFACTOR: everything in one file but a lot functions

--- code/ling_features/compare_ling.py
+++ code/ling_features/compare_ling.py
@@ -8,7 +8,7 @@ import json
 import numpy as np
 from scipy.spatial.distance import cosine, jensenshannon
 from collections import defaultdict
-from helpers import detect_change_point, synt_group
+import ruptures as rpt
 
 groups = json.load(open("../../data/features/synt_groups.json", "r"))
 feature_to_group = {}
@@ -17,6 +17,93 @@ for k, v in groups.items():
         feature_to_group[f] = k
 
 
+
+def detect_change_point(sequence, n_chp=1):
+    """
+    Detects the indices of change points in a sequence of values
+    """
+    sequence = np.array(sequence)
+    algo = rpt.Dynp(model="rbf", jump=1).fit(sequence)
+    chp_index, length = algo.predict(n_bkps=n_chp)
+    return chp_index
+
+
+def synt_group(properties, filtering):
+    informative = [
+        "nominal_function",
+        "function",
+        "modifier",
+        "nominal_modifier",
+        "core_nominals",
+        "nominal_dependents",
+    ]
+
+    new_properties = defaultdict(int)
+    for current_feature, count in properties.items():
+        group = feature_to_group[current_feature.split(":")[0]]
+        if filtering == "group":
+            new_properties[group] += count
+        elif filtering == "partial":
+            group = feature_to_group[current_feature.split(":")[0]]
+            if group in informative:
+                new_properties[current_feature] = count
+            else:
+                new_properties[group] += count
+        elif filtering == "delete":
+            if group in informative:
+                new_properties[current_feature] = count
+        else:
+            raise NotImplementedError
+    return new_properties
+
+def feature_separation(word_properties):
+    properties = defaultdict(int)
+    for el in word_properties:
+        for feat in el.split("|"):
+            properties[feat] += word_properties[el]
+                                                
+    return properties
+
+
+def print_results(words, output, changepoint):
+    with open(f"{output}_graded.tsv", "w") as f:
+            for w in words:
+                f.write(f"{w}\t{words[w]}\n")
+
+    with open(f"{output}_binary.tsv", "w") as f:
+            values = sorted(words, key=words.get, reverse=True)
+            if changepoint == "automatic":
+                threshold = detect_change_point([words[el] for el in values]) + 1
+                #logger.info(f"Change point found at {threshold}")
+            elif changepoint == "half":
+                threshold = int(len(values) / 2)
+            elif changepoint == "semeval":
+                threshold = int(len(values) * 0.43)
+            for val in values[:threshold]:
+                f.write(f"{val}\t1\n")
+            for val in values[threshold:]:
+                f.write(f"{val}\t0\n")
+
+    
+def collect_word_properties(properties):
+    props = defaultdict(lambda: defaultdict(int))
+    for features, count in properties.items():
+        separate_features = features.split("|")
+        for feat in separate_features:
+            try:
+                k, v = feat.split("=")
+            except:
+                continue
+            else:
+                props[k][v] += count
+    return props
+
+def find_features(p1, p2, threshold):
+    features = list(p1.keys() | p2.keys())
+    prop_count = {k: p1.get(k, 0) + p2.get(k, 0) for k in features}
+    total = sum(prop_count.values())
+    return [f for f in features if prop_count[f] / total * 100 > threshold]
+    
 if __name__ == "__main__":
     logging.basicConfig(
         format="%(asctime)s : %(levelname)s : %(message)s", level=logging.INFO
@@ -58,17 +145,25 @@ if __name__ == "__main__":
         "--separation",
         "-s",
         help="Morphological feature separation by |",
-        choices=["yes", "no"],
+        choices=["yes", "no", "2step"],
         default="no",
+    )
+    arg(
+        "--agregate",
+        "-a",
+        help="If separation is '2step' distances are computed separately and then aggregated using either maximum or average",
+        choices=["max", "avg"],
+        default="max",
     )
     arg(
         "--changepoint",
         "-cp",
-        help="How to detect the change point in cosine distance sequences?",
+        help="How to detect the change point in distance sequences?",
         choices=["semeval", "half", "automatic"],
         default="automatic",
     )
 
+    
     args = parser.parse_args()
 
     with open(args.input1, "r") as f:
@@ -83,81 +178,78 @@ if __name__ == "__main__":
     all_features = set()
 
     for word in words:
-        if args.separation == "yes":
-            p1 = defaultdict(int)
-            p2 = defaultdict(int)
-            for el in properties_1[word]:
-                if "|" in el:
-                    separate_features = el.split("|")
-                    for feat in separate_features:
-                        p1[feat] += properties_1[word][el]
+        if args.separation == "2step":
+
+                distance = {}
+                
+                p1 = collect_word_properties(properties_1[word])
+                p2 = collect_word_properties(properties_2[word])
+                
+                feature_classes = list(p1.keys() | p2.keys())
+                
+                for f_class in feature_classes:
+                    features = find_features(p1[f_class], p2[f_class], args.threshold)
+                    vector_1 = np.zeros(len(features))
+                    vector_2 = np.zeros(len(features))
+
+                    for nr, feature in enumerate(features):
+                        vector_1[nr] = p1[f_class][feature]
+                        vector_2[nr] = p2[f_class][feature]
+        
+                    if args.distance == "cos":
+                        distance[f_class] = cosine(vector_1, vector_2)
+                    elif args.distance == "jsd":
+                        distance[f_class] = jensenshannon(vector_1, vector_2)
+                    else:
+                        raise NotImplementedError("Unknown distance: %s" % args.distance)
+        
+                distance = [d for d in distance.values() if not np.isnan(d)]
+        
+                if distance:
+                    if args.agregate == "max":
+                        words[word] = max(distance)
+                    elif args.agregate == "avg":
+                        words[word] = np.mean(distance)
                 else:
-                    p1[el] += properties_1[word][el]
-            for el in properties_2[word]:
-                if "|" in el:
-                    separate_features = el.split("|")
-                    for feat in separate_features:
-                        p2[feat] += properties_2[word][el]
-                else:
-                    p2[el] += properties_2[word][el]
+                    # empty, e.g. german cos, max
+                    words[word] = np.nan
 
         else:
-            p1 = properties_1[word]
-            p2 = properties_2[word]
-
-        if args.filtering != "none":
-            p1 = synt_group(p1, args.filtering, feature_to_group)
-            p2 = synt_group(p2, args.filtering, feature_to_group)
-
-        features = list(p1.keys() | p2.keys())
-
-        prop_count = {k: p1.get(k, 0) + p2.get(k, 0) for k in features}
-        total = sum(prop_count.values())
-        features = [f for f in features if prop_count[f] / total * 100 > args.threshold]
-
-        all_features.update(features)
-
-        vector_1 = np.zeros(len(features))
-        vector_2 = np.zeros(len(features))
-
-        for nr, feature in enumerate(features):
-            try:
-                vector_1[nr] = p1[feature]
-            except KeyError:
-                pass
-            try:
-                vector_2[nr] = p2[feature]
-            except KeyError:
-                pass
-
-        if args.distance == "cos":
-            distance = cosine(vector_1, vector_2)
-        elif args.distance == "jsd":
-            distance = jensenshannon(vector_1, vector_2)
-        else:
-            raise NotImplementedError("Unknown distance: %s" % args.distance)
-
-        if np.isnan(distance):
-            distance = 0.0  # A word was not present in one of the time periods
-        words[word] = distance
+                if args.separation == "yes":
+                    p1 = feature_separation(properties_1[word])
+                    p2 = feature_separation(properties_2[word])           
+                    
+                else:
+                    p1 = properties_1[word]
+                    p2 = properties_2[word]
+        
+                if args.filtering != "none":
+                    p1 = synt_group(p1, args.filtering)
+                    p2 = synt_group(p2, args.filtering)
+        
+                features = find_features(p1, p2, args.threshold)
+        
+                all_features.update(features)
+        
+                vector_1 = np.zeros(len(features))
+                vector_2 = np.zeros(len(features))
+        
+                for nr, feature in enumerate(features):
+                    vector_1[nr] = p1.get(feature,0)
+                    vector_2[nr] = p2.get(feature,1)
+        
+                if args.distance == "cos":
+                    distance = cosine(vector_1, vector_2)
+                elif args.distance == "jsd":
+                    distance = jensenshannon(vector_1, vector_2)
+                else:
+                    raise NotImplementedError("Unknown distance: %s" % args.distance)
+        
+                if np.isnan(distance):
+                    distance = 0.0  # A word was not present in one of the time periods
+                words[word] = distance
 
     if args.output:
-        with open(f"{args.output}_graded.tsv", "w") as f:
-            for w in words:
-                f.write(f"{w}\t{words[w]}\n")
-
-        with open(f"{args.output}_binary.tsv", "w") as f:
-            values = sorted(words, key=words.get, reverse=True)
-            if args.changepoint == "automatic":
-                threshold = detect_change_point([words[el] for el in values]) + 1
-                logger.info(f"Change point found at {threshold}")
-            elif args.changepoint == "half":
-                threshold = int(len(values) / 2)
-            elif args.changepoint == "semeval":
-                threshold = int(len(values) * 0.43)
-            for val in values[:threshold]:
-                f.write(f"{val}\t1\n")
-            for val in values[threshold:]:
-                f.write(f"{val}\t0\n")
+        print_results(words, args.output, args.changepoint)
     else:
         logger.info(words)
--- code/ling_features/evaluate_morph_separation.sh
+++ code/ling_features/evaluate_morph_separation.sh
@@ -9,13 +9,13 @@ do
     for agregate in max avg
     do
 	echo ${agregate}
-	python3 -W ignore compare_morph_separately.py --input1 ../../data/features/${LANG}/corpus1_morph.json --input2 ../../data/features/${LANG}/corpus2_morph.json --output results/${LANG}_separate_${distance}_${agregate} --distance ${distance} --agregate ${agregate}
+	python3 -W ignore compare_ling.py --input1 ../../data/features/${LANG}/corpus1_morph.json --input2 ../../data/features/${LANG}/corpus2_morph.json --output results/${LANG}_separate_${distance}_${agregate} --distance ${distance} --agregate ${agregate} --separation 2step
 
 	python3 ../eval.py results/${LANG}_separate_${distance}_${agregate}_binary.tsv results/${LANG}_separate_${distance}_${agregate}_graded.tsv ../../test_data_truth/task1/${LANG}.txt ../../test_data_truth/task2/${LANG}.txt
 	
 	echo thr 5
     
-	python3 -W ignore compare_morph_separately.py --input1 ../../data/features/${LANG}/corpus1_morph.json --input2 ../../data/features/${LANG}/corpus2_morph.json --output results/${LANG}_separate_${distance}_${agregate}_5 --threshold 5 --distance ${distance} --agregate ${agregate}
+	python3 -W ignore compare_ling.py --input1 ../../data/features/${LANG}/corpus1_morph.json --input2 ../../data/features/${LANG}/corpus2_morph.json --output results/${LANG}_separate_${distance}_${agregate}_5 --threshold 5 --distance ${distance} --agregate ${agregate} --separation 2step
 
 	python3 ../eval.py results/${LANG}_separate_${distance}_${agregate}_5_binary.tsv results/${LANG}_separate_${distance}_${agregate}_5_graded.tsv ../../test_data_truth/task1/${LANG}.txt ../../test_data_truth/task2/${LANG}.txt
 
