9fd9ac704b6469f2e16e1fd239c66b2be1770971
Mario <m.giulianelli.m@gmail.com>
2022-01-31 12:32:37 +0100

    Add max_seq_len argument

--- scripts/finetune-xlmr-en.sh
+++ scripts/finetune-xlmr-en.sh
@@ -9,12 +9,13 @@ source ~/.bash_profile
 # source ${HOME}/projects/erp/venv/bin/activate
 
 # Set cache directory for Huggingface datasets
-export HF_DATASETS_CACHE="/project/dmg_data/lsc/.cache/"
+# export HF_DATASETS_CACHE="/project/dmg_data/lsc/.cache/"
 
 language=english
 model=xlm-roberta-base
 epochs=5
 bsz=4
+max_seq_len=256
 
 python3 -m torch.distributed.launch --nproc_per_node=4 --nnodes=1 --node_rank=0  \
         $HOME/projects/semeval2020/code/xlmr/finetune_mlm.py \
@@ -29,7 +30,7 @@ python3 -m torch.distributed.launch --nproc_per_node=4 --nnodes=1 --node_rank=0 
         --per_device_train_batch_size $bsz \
         --model_name_or_path $model \
 	--overwrite_output_dir \
-	--max_seq_len 256 \
+	--max_seq_len $max_seq_len \
         &> ${HOME}/projects/semeval2020/out/finetune-xlmr-en_$(date +"%FT%T")
 
 
