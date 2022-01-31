e864c86e27987696145d4c33ec93ef7b5e41f346
Mario <m.giulianelli.m@gmail.com>
2022-01-31 12:31:15 +0100

    Comment out irrelevant lines

--- scripts/finetune-xlmr-en.sh
+++ scripts/finetune-xlmr-en.sh
@@ -1,25 +1,20 @@
 #!/bin/bash
-#SBATCH --nodes=1
-#SBATCH --job-name=tune-en
-#SBATCH --time=1:00:00
-#SBATCH --partition=gpu
-#SBATCH --gres=gpu:4
 
 source ~/.bash_profile
 
 # Load all necessary modules.
-echo "Loading modules..."
-module load 2019
+# module load 2019
 
-echo "Loading virtual environment..."
-source ${HOME}/projects/erp/venv/bin/activate
+# Load virtual environment
+# source ${HOME}/projects/erp/venv/bin/activate
 
+# Set cache directory for Huggingface datasets
 export HF_DATASETS_CACHE="/project/dmg_data/lsc/.cache/"
 
 language=english
 model=xlm-roberta-base
-epochs=3
-batch=4
+epochs=5
+bsz=4
 
 python3 -m torch.distributed.launch --nproc_per_node=4 --nnodes=1 --node_rank=0  \
         $HOME/projects/semeval2020/code/xlmr/finetune_mlm.py \
@@ -31,7 +26,7 @@ python3 -m torch.distributed.launch --nproc_per_node=4 --nnodes=1 --node_rank=0 
         --save_steps 100000000 \
         --fp16 \
         --num_train_epochs $epochs \
-        --per_device_train_batch_size $batch \
+        --per_device_train_batch_size $bsz \
         --model_name_or_path $model \
 	--overwrite_output_dir \
 	--max_seq_len 256 \
